eaa326d57b7150994f0fe921613b336ca9e233c4
Ogban-Asuquo Ugot <ogbanugot@gmail.com>
2019-12-06 00:38:47 +0100

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -2,8 +2,6 @@
 
 
 
-# Part 1 - Data Preprocessing
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -30,8 +28,6 @@ X_train, y_train = np.array(X_train), np.array(y_train)
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
 
-
-# Part 2 - Building the RNN
 
 # Importing the Keras libraries and packages
 from keras.models import Sequential
@@ -67,9 +63,6 @@ regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 # Fitting the RNN to the Training set
 regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
 
-
-
-# Part 3 - Making the predictions and visualising the results
 
 # Getting the real stock price of 2017
 dataset_test = pd.read_csv('Google_Stock_Price_Test.csv')
